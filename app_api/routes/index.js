524fcaed8a818972c68c08c4bf5e95603d5f1718
Boggy <lukasbockstaller@gmail.com>
2016-04-23 18:33:19 +0200

    Refactoring

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var ctrlRegistration = require('../controllers/registrations');
-var ctrlOthers = require('../controllers/others');
+
 
 
 //Registration
 router.get('/registration', ctrlRegistration.displayAllRegistrations);
+
 router.get('/registration/:registrationid', ctrlRegistration.displayRegistration);
 
 router.post('/registration', ctrlRegistration.createRegistration);
@@ -14,7 +15,5 @@ router.put('/registration/:registrationid', ctrlRegistration.updateRegistration)
 
 router.delete('/registration/:registrationid', ctrlRegistration.deleteRegistration);
 
-/* Other pages */
-router.get('/about', ctrlOthers.about);
 
 module.exports = router;
