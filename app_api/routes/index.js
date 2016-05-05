76027ce82a45974c339a942d593ffd37002f912c
Boggy <lukasbockstaller@gmail.com>
2016-05-05 18:11:26 +0200

    Heroku fix

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var jwt= require('express-jwt');
 var auth= jwt({
-    secret: process.env.JWT_Secret,
+    secret: process.env.JWT_SECRET,
     personProperty:'payload'
 });
 
